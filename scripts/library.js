5c21d4b4f167e89c0f9fce7b02c6eea20c75bd4b
JacquesFFourie <125289955+JacquesFFourie@users.noreply.github.com>
2023-11-05 20:33:32 +0200

    Year sort and All sort Done
    API Stuff

--- scripts/library.js
+++ scripts/library.js
@@ -2,7 +2,11 @@ $(document).ready(function(){
 
     getApiData();
     getApiHeaderData();
+    //yearSortLastFive();
     
+    
+    
+
     
     
 
@@ -10,7 +14,7 @@ $(document).ready(function(){
 
 function getApiHeaderData(){
 
-    userSavedId = ["tt0499549","tt1825683","tt1211837","tt0800369","tt3895150","tt2245084","tt7078180","tt1772341"]
+    userSavedId = ["tt0499549","tt1825683","tt1211837","tt0800369","tt3895150","tt2245084","tt7078180","tt1772341","tt4975722","tt15398776","tt9362722","tt0816692","tt0468569","tt0993846","tt4154796","tt0944947","tt0068646","tt4574334"]
     const  headerRand = Math.floor(Math.random() * userSavedId.length)
 
     apiUrl = `https://moviesdatabase.p.rapidapi.com/titles/${userSavedId[headerRand]}`
@@ -29,14 +33,7 @@ function getApiHeaderData(){
         };
         
         $.ajax(settings).done(function (response) {
-            //id                dataAr[0]    response.results.id
-            //title             dataAr[1]    response.results.originalTitleText.text
-            //image             dataAr[2]    response.results.primaryImage.url
-            //caption           dataAr[3]    response.results.primaryImage.caption
-            //releaseDate       dataAr[4]    response.results.releaseDate.year
-            
-            
-            
+                    
             
             console.log(response);
             const dataAr = [response.results.id, 
@@ -52,11 +49,10 @@ function getApiHeaderData(){
                 caption: dataAr[3],
                 releaseDate: dataAr[4]
             }))
-            
         
-            
             const headerContainerId = $('#headerContainer');
             const headerImageId = $('#headerImageId')
+            headerContainerId.empty();
             
         
             const headerImage = $(`
@@ -80,18 +76,12 @@ function getApiHeaderData(){
             headerImageId.append(headerImageFinal);
             
         })
-        
 
-    
 }
 
 function getApiData(){
 
-    userSavedId = ["tt0499549","tt1825683","tt1211837","tt0800369","tt3895150","tt2245084","tt7078180","tt1772341"]//userSavedId.length
-
-
-    
-
+    userSavedId = ["tt0499549","tt1825683","tt1211837","tt0800369","tt3895150","tt2245084","tt7078180","tt1772341","tt4975722","tt15398776","tt9362722","tt0816692","tt0468569","tt0993846","tt4154796","tt0944947","tt0068646","tt4574334"]//userSavedId.length ,"tt4975722","tt15398776","tt9362722","tt0816692","tt0468569","tt0993846","tt4154796","tt0944947","tt0068646","tt4574334"
 
 
     for(let i = 0; i < userSavedId.length; i++){
@@ -109,14 +99,7 @@ function getApiData(){
         };
         
         $.ajax(settings).done(function (response) {
-            //id                dataAr[0]    response.results.id
-            //title             dataAr[1]    response.results.originalTitleText.text
-            //image             dataAr[2]    response.results.primaryImage.url
-            //caption           dataAr[3]    response.results.primaryImage.caption
-            //releaseDate       dataAr[4]    response.results.releaseDate.year
-            
-            
-            
+  
             
             console.log(response);
             const dataAr = [response.results.id, 
@@ -133,10 +116,7 @@ function getApiData(){
                 releaseDate: dataAr[4]
             }))
             const cardContainerId = $('#storedTitleCards');
-                    
-            
-                    
-                        
+              
             const card = $(`
             <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
                 <div class="card JcardIn" style="width: 18rem;">
@@ -159,6 +139,308 @@ function getApiData(){
     }
 
    
+}
+getYearList = ["tt0499549","tt1825683","tt1211837","tt0800369","tt3895150","tt2245084","tt7078180","tt1772341","tt4975722","tt15398776","tt9362722","tt0816692","tt0468569","tt0993846","tt4154796","tt0944947","tt0068646","tt4574334"]//
+
+function yearSortLastFive(){
+    sortYear = [2010,2015,2022];
+    releaseYear = []
+    for(let i = 0; i < getYearList.length; i++){
+    apiUrl = `https://moviesdatabase.p.rapidapi.com/titles/${getYearList[i]}`
+    
+    releaseYearMoreFive = []
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: apiUrl,
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '212e6fe1bemsh379babdfd35761ap17f717jsna33b04c724a5',
+            'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+        }
+    };
+    
+    $.ajax(settings).done(function (response) {
+    
+        // console.log(response);
+        const dataAr = [response.results.id, 
+                        response.results.originalTitleText.text, 
+                        response.results.primaryImage.url, 
+                        response.results.primaryImage.caption.plainText, 
+                        response.results.releaseDate.year,
+                        ];
+        const movieMapping = dataAr.map(movieMap =>({
+            id: dataAr[0],
+            title: dataAr[1],
+            image: dataAr[2],
+            caption: dataAr[3],
+            releaseDate: dataAr[4]
+        }))
+
+    if(dataAr[4] == sortYear[0]){
+        cardContainerId.empty();
+        const cardContainerId = $('#storedTitleCards');
+        
+
+        const card = $(`
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="card JcardIn" style="width: 18rem;">
+                    <img src="${dataAr[2]}" class="card-img-top JcardImgTop" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title JcardTH">${dataAr[1]}</h5>
+                    <p class="card-text JcardT">${dataAr[3]}</p>
+                    <a href="#" class="btn btn-primary btnWatchL">Add to Watchlist</a>
+                </div>
+            </div>
+        </div>`)
+
+
+
+        cardContainerId.append(card);
+    }
+          
+    })
+  
+}
+
+
+
+
+
+
+function cardGet(){
+    console.log(releaseYear.length)
+for(let a = 0; a < releaseYear.length; a++){
+  
+      
+    apiUrlF = `https://moviesdatabase.p.rapidapi.com/titles/${releaseYear[a]}`
+    
+
+    const settingsOne = {
+        async: true,
+        crossDomain: true,
+        url: apiUrlF,
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '212e6fe1bemsh379babdfd35761ap17f717jsna33b04c724a5',
+            'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+        }
+    };
+    
+    $.ajax(settingsOne).done(function (response) {
+
+        // console.log(response);
+        const dataAr = [response.results.id, 
+                        response.results.originalTitleText.text, 
+                        response.results.primaryImage.url, 
+                        response.results.primaryImage.caption.plainText, 
+                        response.results.releaseDate.year,
+                        ];
+        const movieMapping = dataAr.map(movieMap =>({
+            id: dataAr[0],
+            title: dataAr[1],
+            image: dataAr[2],
+            caption: dataAr[3],
+            releaseDate: dataAr[4]
+        }))
+
+        const cardContainerId = $('#storedTitleCards');
+        cardContainerId.empty();
+
+        const card = $(`
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="card JcardIn" style="width: 18rem;">
+                    <img src="${dataAr[2]}" class="card-img-top JcardImgTop" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title JcardTH">${dataAr[1]}</h5>
+                    <p class="card-text JcardT">${dataAr[3]}</p>
+                    <a href="#" class="btn btn-primary btnWatchL">Add to Watchlist</a>
+                </div>
+            </div>
+        </div>`)
+
+
+
+        cardContainerId.append(card);
+   
+})
+}
+}
+
+ 
+
+
+}
+// function rating(){
+
+//     getRatingList = ["tt0499549","tt1825683","tt1211837","tt0800369","tt3895150","tt2245084","tt7078180","tt1772341","tt4975722","tt15398776","tt9362722","tt0816692","tt0468569","tt0993846","tt4154796","tt0944947","tt0068646","tt4574334"]
+//     ratingSortAr = [];
+//     aboveEightRating = [];
+//     belowEightRating = [];
+
+
+    // for(let b = 0; b < getRatingList.length; b++){
+    //     const settings = {
+    //         async: true,
+    //         crossDomain: true,
+    //         url: `https://moviesdatabase.p.rapidapi.com/titles/${getRatingList[b]}/ratings`,
+    //         method: 'GET',
+    //         headers: {
+    //             'X-RapidAPI-Key': '7f077e9ed9msh1e0c21025299eaap11a459jsn457e98831728',
+    //             'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+    //         }
+    //     };
+        
+    //     $.ajax(settings).done(function (response) {
+
+    //     const ratingAr = [response.results.averageRating, 
+    //                         response.results.tconst];
+
+    //     // ratingSortAr.push(ratingAr[0])
+    //     // console.log(ratingSortAr)
+
+
+
+    //     if(ratingAr[0] >=8){//Above 8
+    //         //console.log(ratingAr);
+    //         aboveEightRating.push(ratingAr[1])           
+    //     }
+
+    //     });
+        
+            
+        
+        
+
+    //     function eightScore(){
+    //         for(let c = 0; c < aboveEightRating.length; c++){
+    //                 apiUrl = `https://moviesdatabase.p.rapidapi.com/titles/${aboveEightRating[c]}`
+            
+        
+    //         const settingsOne = {
+    //             async: true,
+    //             crossDomain: true,
+    //             url: apiUrl,
+    //             method: 'GET',
+    //             headers: {
+    //                 'X-RapidAPI-Key': 'c8bef0d4c3msh3b946805a8dbf8bp1224a7jsn11220334bd98',
+    //                 'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+    //             }
+    //         };
+            
+    //         $.ajax(settingsOne).done(function (response) {
+        
+    //             // console.log(response);
+    //             const dataAr = [response.results.id, 
+    //                             response.results.originalTitleText.text, 
+    //                             response.results.primaryImage.url, 
+    //                             response.results.primaryImage.caption.plainText, 
+    //                             response.results.releaseDate.year
+    //                             ];
+    //             const movieMapping = dataAr.map(movieMap =>({
+    //                 id: dataAr[0],
+    //                 title: dataAr[1],
+    //                 image: dataAr[2],
+    //                 caption: dataAr[3],
+    //                 releaseDate: dataAr[4]
+    //             }))
+        
+    //             const cardContainerId = $('#storedTitleCards');
+    //             // cardContainerId.empty();
+        
+    //             const card = $(`
+    //             <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+    //                 <div class="card JcardIn" style="width: 18rem;">
+    //                         <img src="${dataAr[2]}" class="card-img-top JcardImgTop" alt="...">
+    //                         <div class="card-body">
+    //                         <h5 class="card-title JcardTH">${dataAr[1]}</h5>
+    //                         <p class="card-text JcardT">${dataAr[3]}</p>
+    //                         <a href="#" class="btn btn-primary btnWatchL">Add to Watchlist</a>
+    //                     </div>
+    //                 </div>
+    //             </div>`)
+        
+        
+        
+    //             cardContainerId.append(card);
+           
+    //     })
+    //     }
+    // }
+
+// }
+
+   
+    
+
+    // }       
+//     function eightScore(){
+//         for(let c = 0; c < aboveEightRating.length; c++){
+//                 apiUrl = `https://moviesdatabase.p.rapidapi.com/titles/${aboveEightRating[c]}`
+        
+    
+//         const settingsOne = {
+//             async: true,
+//             crossDomain: true,
+//             url: apiUrl,
+//             method: 'GET',
+//             headers: {
+//                 'X-RapidAPI-Key': '7f077e9ed9msh1e0c21025299eaap11a459jsn457e98831728',
+//                 'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+//             }
+//         };
+        
+//         $.ajax(settingsOne).done(function (response) {
+    
+//             // console.log(response);
+//             const dataAr = [response.results.id, 
+//                             response.results.originalTitleText.text, 
+//                             response.results.primaryImage.url, 
+//                             response.results.primaryImage.caption.plainText, 
+//                             response.results.releaseDate.year
+//                             ];
+//             const movieMapping = dataAr.map(movieMap =>({
+//                 id: dataAr[0],
+//                 title: dataAr[1],
+//                 image: dataAr[2],
+//                 caption: dataAr[3],
+//                 releaseDate: dataAr[4]
+//             }))
+    
+//             const cardContainerId = $('#storedTitleCards');
+//             // cardContainerId.empty();
+    
+//             const card = $(`
+//             <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+//                 <div class="card JcardIn" style="width: 18rem;">
+//                         <img src="${dataAr[2]}" class="card-img-top JcardImgTop" alt="...">
+//                         <div class="card-body">
+//                         <h5 class="card-title JcardTH">${dataAr[1]}</h5>
+//                         <p class="card-text JcardT">${dataAr[3]}</p>
+//                         <a href="#" class="btn btn-primary btnWatchL">Add to Watchlist</a>
+//                     </div>
+//                 </div>
+//             </div>`)
+    
+    
+    
+//             cardContainerId.append(card);
+       
+//     })
+//     }
+// }                      
+
+
+
+
+
+
+
+
+function emptyCard(){
+    const cardContainerId = $('#storedTitleCards');
+    cardContainerId.empty()
+    
 }
 
 
