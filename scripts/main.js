4e8121e851e3c4017690e0c88c139283af153e4b
V24code <190243@virtualwindow.co.za>
2023-11-06 13:02:49 +0200

    watchlist button
    update2

--- scripts/main.js
+++ scripts/main.js
@@ -21,6 +21,40 @@
         });
     });
 
+     // Function to add "The Joker" to local storage
+  function addToWatchlist() {
+    // Create a JavaScript object with movie information
+    const jokerMovie = {
+      title: "The Joker",
+      director: "Todd Phillips",
+      year: 2019, // Update the year to the correct one
+      rating: 8.5, // Add the correct rating
+    };
+
+    // Get the current watchlist from local storage (if any)
+    let watchlist = JSON.parse(localStorage.getItem('watchlist')) || [];
+
+    // Check if "The Joker" is already in the watchlist
+    const isJokerInWatchlist = watchlist.some(movie => movie.title === 'The Joker');
+
+    if (!isJokerInWatchlist) {
+      // If not in the watchlist, add it
+      watchlist.push(jokerMovie);
+
+      // Update the watchlist in local storage
+      localStorage.setItem('watchlist', JSON.stringify(watchlist));
+
+      // Provide some feedback to the user
+      alert('The Joker has been added to your watchlist.');
+    } else {
+      alert('The Joker is already in your watchlist.');
+    }
+  }
+
+  // Add a click event handler to the "Add to Watchlist" button
+  const addToWatchlistButton = document.getElementById('add-to-watchlist');
+  addToWatchlistButton.addEventListener('click', addToWatchlist);
+
 
         
         
